1fa4c51915960255dc509d9309cbe33620ae4350
ilwindcunha <ilwindcunha@gmail.com>
2018-06-07 18:15:38 -0500

    committing pypyodbc

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
-# import pypyodbc
+import pypyodbc
 #import sqlite3 as sql
-import pyodbc
+# import pyodbc
 import csv
 import os
 
@@ -15,10 +15,10 @@ database = 'ilwin'
 username = 'ilwin'
 password = 'esxi@S5n'
 driver = '{SQL Server}'
-# cnxn = pypyodbc.connect("Driver={ODBC Driver 13 for SQL Server};"
-#                         "Server=tcp:ilwin.database.windows.net;Database=ilwin;Uid=ilwin;Pwd=esxi@S5n;")
-cnxn = pyodbc.connect(
-    'DRIVER=' + driver + ';PORT=1433;SERVER=' + server + ';PORT=1443;DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+cnxn = pypyodbc.connect("Driver={ODBC Driver 13 for SQL Server};"
+                        "Server=tcp:ilwin.database.windows.net;Database=ilwin;Uid=ilwin;Pwd=esxi@S5n;")
+# cnxn = pyodbc.connect(
+#     'DRIVER=' + driver + ';PORT=1433;SERVER=' + server + ';PORT=1443;DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
 cursor = cnxn.cursor()
 
 
