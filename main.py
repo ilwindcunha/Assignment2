0bd334a87ac37f6fa22013ab411bf19401b1c5d8
ilwindcunha <ilwindcunha@gmail.com>
2018-06-09 18:09:07 -0500

    committing pypyodbc

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 import pypyodbc
 #import sqlite3 as sql
-# import pyodbc
+import pyodbc
+import datetime as DT
 import csv
 import os
+import datetime
+
 
 app = Flask(__name__)
 
@@ -40,7 +43,7 @@ def uploadCSV():
         reader = csv.reader(f)
         columns = next(reader)
         print(columns)
-        query = 'insert into EarthquakeOne ({0}) values ({1})'
+        query = 'insert into EarthquakeTwo ({0}) values ({1})'
         query = query.format(','.join(columns), ','.join('?' * len(columns)))
 
         for data in reader:
@@ -63,7 +66,7 @@ def addrec():
 
     if request.method == 'POST':
         # print("inside")
-        query = "select count(*) from earthquakeone where mag > 5.0"
+        query = "select count(*) from EarthquakeTwo where mag > 5.0"
         print(query)
         cursor.execute(query)
         result=cursor.fetchone()
@@ -74,6 +77,44 @@ def addrec():
 
         return render_template("view.html", msg=value)
 
+@app.route('/search', methods=['POST', 'GET'])
+def search():
+    # print("here")
+    # Search    for 2.0 to 2.5, 2.5 to 3.0… for a week a day or the whole 30 days.
+    if request.method == 'POST':
+        # print("inside")
+        rangeOne = request.form['range1']
+        rangeTwo = request.form['range2']
+        length = request.form['length']
+        print(length)
+        if(length=='week'):
+            today = DT.date.today()
+            criteria=today-DT.timedelta(days=7)
+        if(length=='day'):
+            today = DT.date.today()
+            criteria = today - DT.timedelta(days=1)
+        if(length=='month'):
+            today = DT.date.today()
+            criteria = today - DT.timedelta(days=30)
+
+        print(today)
+        print (criteria)
+
+
+        # stringToday = today.dateutcnow().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+        # stringCriteria = criteria.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+
+        # query = "select * from EarthquakeTwo where (timee between "+today +" and "+criteria+") and (mag between '"+rangeTwo+"' and '"+rangeTwo+"')"
+        query = "select * from EarthquakeTwo where timee between ? and ? "
+        print(query)
+        cursor.execute(query,(today,criteria))
+        result=cursor.fetchone()
+        print("code here")
+        print(result)
+        value=result[0]
+        print(value)
+
+        return render_template("view.html", msg=value)
 
 
 if __name__ == '__main__':
